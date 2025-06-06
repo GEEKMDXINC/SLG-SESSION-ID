9b180aa0248dda3aaf2ba9c1619e731f7c426dbd
GEEKMDXINC <herukobot67@gmail.com>
2025-06-06 19:32:27 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const { exec } = require("child_process");
 let router = express.Router()
 const pino = require("pino");
