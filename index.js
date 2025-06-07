2d8f3f60d70ea3a950c978ba7ce4bf5a19aff7d1
GEEKMDXINC <herukobot67@gmail.com>
2025-06-07 07:39:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // ici c'est l'index 
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8000;
+
+let code = require('./pair');
+const router = require('./qr');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/code', code);
+app.use('/qr', router);
+
+app.use('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.use('/qrcode', (req, res) => {
+    res.sendFile(path.join(__dirname,'qr.html'));
+});
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+});
+
+module.exports = app;
