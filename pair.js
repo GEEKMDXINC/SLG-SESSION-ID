d1e03d3f377501dfff9563d180c9551cf4e98771
GEEKMDXINC <herukobot67@gmail.com>
2025-06-18 14:15:05 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -34,7 +34,7 @@ if (fs.existsSync('./session')) {
                 },
                 printQRInTerminal: false,
                 logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: Browsers.macOS("Safari"),
+                browser: Browsers.macOS("Chrome"),
             });
 
             if (!slg.authState.creds.registered) {
