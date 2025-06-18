3f3f0cc6eb0bfc7eb0f59882451e85c5dc93211d
GEEKMDXINC <herukobot67@gmail.com>
2025-06-18 14:16:10 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -39,7 +39,7 @@ app.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: Browsers.macOS("Safari"),
+                browser: Browsers.macOS("Desktop"),
             });
 
             const qrOptions = {
